d6f21b30690b7fc5ec51e4155a7f453edc126478
Adrian McCarroll <amccarroll@outlook.com>
2014-08-03 11:27:51 +0100

    version 3.2 of rails user authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '5ef3dcd9d1caa8a9af3c1dd6a3e6fb91e72c51a4f4705adab622baa7d5df2a3876b6049961d3d9613f7ccd4801487a8b1a2972d925d5e193bd53425d08f11679'
